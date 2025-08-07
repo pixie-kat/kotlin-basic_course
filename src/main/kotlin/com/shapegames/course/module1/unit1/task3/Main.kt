6f24eb0c7c1a73b79217c56816c90206f5d4bff9
Kathrin Knudsen <Kathrin@shape.dk>
2025-08-07 16:42:41 +0200

    clean slate on master

--- src/main/kotlin/com/shapegames/course/module1/unit1/task3/Main.kt
+++ src/main/kotlin/com/shapegames/course/module1/unit1/task3/Main.kt
@@ -5,10 +5,3 @@ Assignment 3: Arithmetic Operators
 Task:
 Create width and height variables. Find the area of a rectangle.
 */
-
-fun main() {
-    val width = 3;
-    val height = 6;
-    val area = width * height
-    println("The area of a rectangle is width x height. In this example it is $area")
-}
