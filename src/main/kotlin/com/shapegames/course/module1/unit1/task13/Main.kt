77a5e4676c6857eb7357b10c1e9c22d3da2c5f42
Kathrin Knudsen <Kathrin@shape.dk>
2025-08-07 15:58:48 +0200

    added solution for task 13

--- src/main/kotlin/com/shapegames/course/module1/unit1/task13/Main.kt
+++ src/main/kotlin/com/shapegames/course/module1/unit1/task13/Main.kt
@@ -5,3 +5,16 @@ Assignment 13: Combination of Operators
 Task:
 Count apples in baskets and some extra apples.
 */
+fun main() {
+    println("How many apples are in the basket?")
+    val applesInBasket = readln().toInt()
+    println("There are ${applesInBasket} apples in the basket")
+    println("Do you want more apples? y/n")
+    val moreApples = readln()
+    if (moreApples == "y")  {
+        println ("How many more apples do you want?")
+        val furtherApples = readln().toInt()
+        println("There are in total ${applesInBasket + furtherApples} apples")
+    }
+    else println("There are in total $applesInBasket apples")
+}
