6699510b939909aec17326d57124b734421fdddd
Kathrin Knudsen <Kathrin@shape.dk>
2025-08-07 15:23:36 +0200

    Improved task 1 and added solution for task 9

--- src/main/kotlin/com/shapegames/course/module1/unit1/task9/Main.kt
+++ src/main/kotlin/com/shapegames/course/module1/unit1/task9/Main.kt
@@ -5,3 +5,10 @@ Assignment 9: Comparison Operators
 Task:
 Check if myAge is enough to meet requiredAge.
 */
+fun main(){
+    val requiredAge = 21
+    println("What is your age?")
+    val myAge = readln()
+    val isAgeGreater = myAge.toInt() >= requiredAge
+    println ("Are you older than the required age? $isAgeGreater")
+}
