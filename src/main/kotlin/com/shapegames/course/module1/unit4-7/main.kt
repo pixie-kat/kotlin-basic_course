56242c74fab42671cdf9ccbd5e2a0e40c3224690
Kathrin Knudsen <Kathrin@shape.dk>
2025-08-13 13:07:02 +0200

    when statement tasks

--- src/main/kotlin/com/shapegames/course/module1/unit4-7/main.kt
+++ src/main/kotlin/com/shapegames/course/module1/unit4-7/main.kt
@@ -1,8 +1,9 @@
 package org.example.com.shapegames.course.module1.`unit4-7`
 
 fun main() {
- task1IF()
+    task2WHEN()
 }
+//IF expressions
 /*Task 1 IF:
 Write a program that checks if a number is positive, negative, or zero.
 Print a message for each case.
@@ -37,4 +38,47 @@ fun task2IF() {
         println("The number $number is an odd number")
     }
 
+}
+//when expressions
+/*
+Task 1:
+Write a program that takes a variable grade (Int) from 1 to 5 and prints:
+
+"Excellent" for 5
+"Good" for 4
+"Okay" for 3
+"Bad" for 2
+"Terrible" for 1
+val grade = 4
+*/
+
+fun task1WHEN(){
+    val grade = 4
+    when (grade) {
+        5 -> println("Excellent")
+        4 -> println("Good")
+        3 -> println("Okay")
+        2 -> println("Bad")
+        1 -> println("Terrible")
+        else -> println("Grade unknown")
+    }
+
+}
+
+/*
+Task 2:
+Take a variable month (Int) from 1 to 12 and print the season ("Winter", "Spring", "Summer", "Autumn").
+Use when and ranges.
+
+val month = 7
+ */
+
+fun task2WHEN(){
+    val month = 7
+    when (month){
+        in 1 .. 3 -> println("Winter")
+        in 4 ..6 -> println("Spring")
+        in 7 .. 9 -> println("Summer")
+        in 10 .. 12 -> println("Autumn")
+    }
 }
