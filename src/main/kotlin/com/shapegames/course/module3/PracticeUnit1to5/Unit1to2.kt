625c5714f2a354c28033da38111b77713ba6d06f
Kathrin Knudsen <kathrin@fyhr.dk>
2025-09-07 21:54:46 +0200

    task 3 of the module/chapter 3 homework

--- src/main/kotlin/com/shapegames/course/module3/PracticeUnit1to5/Unit1to2.kt
+++ src/main/kotlin/com/shapegames/course/module3/PracticeUnit1to5/Unit1to2.kt
@@ -72,6 +72,7 @@ class Book(
 //Create another package called stringops with a function called concat(a: String, b: String): String that joins two strings. Import both add and concat into a third file and use both functions.
 
 
+
 /*
 Unit 4: Getters and Setters
 Task 1:
