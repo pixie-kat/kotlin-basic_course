a0f56c2a322ec93a0b4bb4c4481cacea736bc5db
Kathrin Knudsen <kathrin@fyhr.dk>
2025-09-09 14:09:10 +0200

    fixed task description

--- src/main/kotlin/com/shapegames/course/module3/PracticeUnit1to7/Task7MedicalAccountNotifications/MedicalApp.kt
+++ src/main/kotlin/com/shapegames/course/module3/PracticeUnit1to7/Task7MedicalAccountNotifications/MedicalApp.kt
@@ -1,6 +1,6 @@
 package com.shapegames.course.module3.PracticeUnit1to7.Task7MedicalAccountNotifications
 
-
+//Create objects of each type and call their alert() function to see the different messages.
 fun main() {
     val medicalAccountInfo = MedicalAccount("Kathrin")
     medicalAccountInfo.alert()
@@ -14,22 +14,21 @@ fun main() {
 //
 //Suppose you are working on a medical app.
 //
-//Create a base class MedicalAccount with a property username (String) and a function notify() that prints "General medical notification for {username}".
+//Create a base class MedicalAccount with a property username (String) and a function alert() that prints "General medical notification for {username}".
 open class MedicalAccount(val username: String){
     open fun alert(){
         println("General medical notification for ${username}")
     }
 }
-//Create a class DoctorAccount that inherits from MedicalAccount and overrides the notify() function to print "Doctor alert for {username}".
+//Create a class DoctorAccount that inherits from MedicalAccount and overrides the alert() function to print "Doctor alert for {username}".
 class DoctorAccount(username: String) : MedicalAccount(username){
     override fun alert() {
         println("Doctor alert for ${username}")
     }
 }
-//Create a class PatientAccount that also inherits from MedicalAccount and overrides the notify() function to print "Patient reminder for {username}".
+//Create a class PatientAccount that also inherits from MedicalAccount and overrides the alert() function to print "Patient reminder for {username}".
 class PatientAccount(username: String): MedicalAccount(username){
     override fun alert() {
         println("Patient reminder for ${username}")
     }
 }
-//Create objects of each type and call their notify() function to see the different messages.
