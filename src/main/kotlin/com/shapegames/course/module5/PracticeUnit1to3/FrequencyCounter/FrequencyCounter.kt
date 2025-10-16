3ea15483ef9ec91cb314866fc3d0c1b1ea2f04b3
Kathrin Knudsen <kathrin@fyhr.dk>
2025-10-16 10:27:18 +0200

    setting up FrequencyCounter

---
+++ src/main/kotlin/com/shapegames/course/module5/PracticeUnit1to3/FrequencyCounter/FrequencyCounter.kt
@@ -0,0 +1,11 @@
+package com.shapegames.course.module5.PracticeUnit1to3.FrequencyCounter
+
+/*
+2. Frequency Counter
+Create a class called FrequencyCounter.
+Write a function that takes a list of words and returns a map with each word and the number of times it appears in the list.
+ */
+
+class FrequencyCounter {
+
+}
